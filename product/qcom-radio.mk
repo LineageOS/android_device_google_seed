94d3faeb921a057f7ceb4225322907d6cbf608fd
Arvin Quilao <arquilao@gmail.com>
2017-06-15 06:48:54 +0000

    Unify Android One variants
    The device tree should support all 4 variants of Android One 2nd Gen.
    Use init library to set the corresponding properties and build fingerprints.
    
    Change-Id: I4173a68aa68d5bc949881dd21cd76511e864b2ad

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,11 +1,9 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.radio.multisim.config=dsds \
     persist.radio.custom_ecc=1 \
     persist.radio.ecc_hard_1=998 \
     persist.radio.ecc_hard_count=1 \
-    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so \
-    ro.telephony.default_network=9,9
+    rild.libpath=/system/vendor/lib/libril-qc-qmi-1.so
 
 # RIL
 ifeq ($(QCPATH),)
