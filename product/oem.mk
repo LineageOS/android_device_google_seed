61a3b0f5da543592746fceaa608d3d62c4d0f798
Arvin Quilao <arquilao@gmail.com>
2017-06-28 10:01:52 +0000

    seed: Trim down the blacklist
    These properties are set via init binary.
    
    Change-Id: I3299201ff91f11f1e9f085c8346d33827e19b01e

--- product/oem.mk
+++ product/oem.mk
@@ -1,8 +1,5 @@
 # Delegation for OEM customization
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
-    ro.build.description \
-    ro.build.fingerprint \
-    ro.product.device \
     ro.product.name \
     ro.product.manufacturer \
     ro.product.model
